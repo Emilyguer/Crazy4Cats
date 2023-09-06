d07be185e4b46ba357e5a81b8e10b14550eabff1
Emiguer <emilyguerrero@gmail.com>
2023-09-06 12:26:31 -0300

    Feat:(Creacion del controlador comment y configuraciones vistas)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
+  resources :comments, only: [:create]
+
+  post '/new_user_reaction', to: 'reactions#new_user_reaction', as:
+  'new_user_reaction'
+  
 
   # Establece la ruta raíz en "posts#index"
   root "posts#index"
